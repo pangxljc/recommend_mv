c2ce183248c6db0c3b20fe15c82735e18dcbf94e
v-shluo <v-shluo@microsoft.com>
2018-05-05 06:27:49 +0800

    add spark_movie_caculate

--- src/main/java/service/BrowseService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import po.Browse;
-
-
-public interface BrowseService {
-    // 根据用户id获取Browse记录
-    Browse getBrowseByUserId(Integer userid);
-}
--- src/main/java/service/CategoryService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import common.E3Result;
-
-
-public interface CategoryService {
-    //获取所有分类标签
-    E3Result GetAllCategory();
-}
